d3c9970e7ed737163de48ae1f03165ea3ed183d1
anandshankarbala <anandshankarbala@gmail.com>
2020-05-02 19:45:25 +0530

    bala Icon

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import rickRollImage from './img/rick-astley.gif';
+import balaIcon from './img/balaIcon.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={rickRollImage} className="App-logo" alt="logo" />
-        <p>Lets start rolling</p>
+        <img src={balaIcon} className="App-logo" alt="logo" />
+        <p>N.BALASUBRAMANIAN</p>
+        <p>Advisor:Investments/Insurance<div>blog: yourbala63.blogspot.in</div></p>
       </header>
     </div>
   );
