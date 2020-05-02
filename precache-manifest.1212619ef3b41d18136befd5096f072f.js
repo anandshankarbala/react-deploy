76d05d8b6fcea29fcc3a81e6a93c833a258c57b8
anandshankarbala <anandshankarbala@gmail.com>
2020-05-02 16:56:19 +0530

    Updates

---
+++ precache-manifest.1212619ef3b41d18136befd5096f072f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ef2882f2d0c26b8cc26fc76bf66e8b",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "e70fba293c0c4f596d42",
+    "url": "/react-deploy/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "a1bc7dcb0ddda7c2efa0",
+    "url": "/react-deploy/static/js/2.d6d43cce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.d6d43cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e70fba293c0c4f596d42",
+    "url": "/react-deploy/static/js/main.f3e4b14b.chunk.js"
+  },
+  {
+    "revision": "4d275ecffff8f8311b08",
+    "url": "/react-deploy/static/js/runtime-main.8e4be4b4.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-deploy/static/media/logo.ee7cd8ed.svg"
+  }
+]);
